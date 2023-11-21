210712a5ed3261e9805f226e82b5f05e5e976d6b
letiiita <lrios4400@stu.d214.org>
2023-11-21 10:14:43 -0600

    Initial Commit

---
+++ Movie API/Movie_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_APIApp.swift
+//  Movie API
+//
+//  Created by Leticia Rios on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Movie_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Movie APIUITests/Movie_APIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Movie_APIUITestsLaunchTests.swift
+//  Movie APIUITests
+//
+//  Created by Leticia Rios on 11/21/23.
+//
+
+import XCTest
+
+final class Movie_APIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
